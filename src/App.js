e06425a9f98555a2a0e3cc48bba95b8793c5df4f
Hannah Rose <52452654+HannahRose578@users.noreply.github.com>
2023-12-06 22:15:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+// App Component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+  const [loginAttemptFailed, setLoginAttemptFailed] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Assuming 'admin' as the correct username and 'password' as the correct password
+    if (username === 'admin' && password === 'password') {
+      setUserIsLoggedIn(true);
+      setLoginAttemptFailed(false);
+    } else {
+      setLoginAttemptFailed(true);
+    }
+  };
+
+  const handleLogout = () => {
+    setUserIsLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
+  if (userIsLoggedIn) {
+    // Successful Login Screen
+    return (
+      <div>
+        <h1>Welcome! You have successfully logged in.</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  } else if (loginAttemptFailed) {
+    // Unsuccessful Login Screen
+    return (
+      <div>
+        <h1>Login Unsuccessful, please try again.</h1>
+        <button onClick={() => setLoginAttemptFailed(false)}>Back to Login Page</button>
+      </div>
+    );
+  } else {
+    // Login Screen
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={handleUsernameChange} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
